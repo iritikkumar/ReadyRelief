46fa863750ac690482f356b511962e773c8a0d8e
Ritik Kumar <thakurritik558@gmail.com>
2023-03-24 23:44:36 +0530

    Initialised Directories and Database Connection is made

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+mongoose.set('strictQuery', true);
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URL, {
+    dbName: "ReadyRelief",
+}).then(()=> { console.log("DB Connection Successfull!")})
+    .catch((err)=> {
+        console.log(err);
+}); 
+
+app.use((err,req,res,next)=>{
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+        success: false,
+        status,
+        message
+    })
+});
+
+
+app.use(express.json());
+app.listen(port, ()=>{
+    console.log(`Server is listening to port ${port}`);
+})

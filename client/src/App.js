df1b1296ee9ce318cc6dd1baa5801e70a6c210b6
Ritik Kumar <thakurritik558@gmail.com>
2023-03-25 12:01:42 +0530

    login/signup frontend is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Login from './pages/Login';
+import Register from "./pages/Register"
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello</h1>
-    </div>
+    // <div className="App">
+    // <h1>Hello</h1>
+    // </div>
+    <Login/>
+
   );
 }
 
